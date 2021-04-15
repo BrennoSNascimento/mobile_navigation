701b959de516485481dfe1d24e2dd97abfb9d5bf
BrennoSNascimento <brennosilveira201@gmail.com>
2021-04-15 18:18:04 -0300

    uso simples de navigation

---
+++ app/src/main/java/com/example/teste_navigation/presentation/SecondFragment.kt
@@ -0,0 +1,30 @@
+package com.example.teste_navigation.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.teste_navigation.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val  button = view.findViewById<Button>(R.id.nextButton)
+        button.setOnClickListener { this.findNavController().navigate(R.id.action_secondFragment_to_thirdFragment) }
+
+
+    }
+
+}
